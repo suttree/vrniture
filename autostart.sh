fe2b9a08e048c2130a704d25a6e737d33aac3191
suttree <duncan@suttree.com>
2017-12-31 12:59:50 +0000

    autostartup

--- autostart.sh
+++ autostart.sh
@@ -1,1 +1,1 @@
-/usr/local/bin/processing-java --sketch=/home/pi/src/vrniture/processing/circles/ --run
+/usr/local/bin/processing-java --sketch=/home/pi/src/vrniture/processing/circles/ --run >> /home/pi/src/vrniture/autostart.log
