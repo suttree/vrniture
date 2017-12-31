1de862daeb71423e9eb6ca2d7d333e39df27cd7a
suttree <duncan@suttree.com>
2017-12-31 11:42:24 +0000

    rename for autostart consitency

--- server.sh
+++ server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "Starting server: http://0.0.0.0:8000/"
-`chromium-browser --kiosk http://0.0.0.0:8000/index1.html`
+#echo "Starting server: http://0.0.0.0:8000/"
+#`chromium-browser --kiosk http://0.0.0.0:8000/index1.html`
 python -m SimpleHTTPServer 8000
