ba2cf4c6f27c61c8a1de93354a40b3266cc75ddd
Duncan Gough <duncan@suttree.com>
2018-01-02 13:57:20 +0000

    more instructions

--- blankscreen.sh
+++ blankscreen.sh
@@ -1,3 +1,13 @@
+# From http://www.geeks3d.com/hacklab/20160108/how-to-disable-the-blank-screen-on-raspberry-pi-raspbian/
+
+# disable temporarily:
 sudo xset s off
 sudo xset -dpms
 sudo xset s noblank
+
+# disable forever:
+# sudo vi/etc/lightdm/lightdm.conf
+#
+# add to SeatDefaults:
+# [SeatDefaults]
+# xserver-command=X -s 0 -dpms
