edc0b146beb05f2a95ae42fe7f4ee9ac490f856b
pi <duncan@suttree.com>
2019-05-28 20:42:45 +0100

    updated run script with more sketches

--- vrn-run.py
+++ vrn-run.py
@@ -14,6 +14,17 @@ sketches = [
     'maylers',
     'purple_squares',
     'led',
+    'back2',
+    'circles',
+    'shapes',
+    'shapes2',
+    'special_branch_layared_squares',
+    'special_branch_layers',
+    'special_branch_natural_squares',
+    'special_branch_perlin_shapes',
+    'special_branch_shoreline',
+    'special_branch_stage_lighting',
+    'special_branch_terrain',
 ]
 
 sketch = 'DISPLAY=:0 /usr/local/bin/processing-java --sketch="/home/pi/src/vrniture/processing/{0}/" --run'.format(random.choice(sketches))
