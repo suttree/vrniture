e03b845217ad707a913a996c5d0d76ff355be3a7
Duncan Gough <duncan.gough@analogfolk.com>
2019-07-10 09:56:03 +0100

    updated run script, time aware

--- vrn-run.py
+++ vrn-run.py
@@ -1,34 +1,66 @@
 #! /usr/bin/env python
 
-import os
-import random
-
 # TODO: run via cron every 5 minutes, and use random.choice() to either quit or run
 
+import sys
 
-# TODO: find a cleaner way to do this
-os.system('killall java')
+def main():
+    if shutdown_at_nighttime():
+        sys.exit()
 
-sketches = [ 
-    'stare',
-    'maylers',
-    'purple_squares',
-    'led',
-    'back2',
-    'circles',
-    'shapes',
-    'shapes2',
-    'special_branch_layered_squares',
-    'special_branch_layers',
-    'special_branch_natural_squares',
-    'special_branch_perlin_shapes',
-    'special_branch_shoreline',
-    'special_branch_stage_lighting',
-    'special_branch_terrain',
-    'coloured_squares',
-]
+    import os
+    import random
+    import datetime
 
-sketch = 'DISPLAY=:0 /usr/local/bin/processing-java --sketch="/home/pi/src/vrniture/processing/{0}/" --run'.format(random.choice(sketches))
+    # TODO: select scripts based on this? or dim screen...
+    day_part = get_part_of_day(datetime.now().hour)
 
-print(sketch)
-os.system(sketch)    
+    sketches = [ 
+        'stare',
+        'maylers',
+        'purple_squares',
+        'led',
+        'back2',
+        'circles',
+        'shapes',
+        'shapes2',
+        'special_branch_layered_squares',
+        'special_branch_layers',
+        'special_branch_natural_squares',
+        'special_branch_perlin_shapes',
+        'special_branch_shoreline',
+        'special_branch_stage_lighting',
+        'special_branch_terrain',
+        'coloured_squares',
+    ]
+
+    sketch = 'DISPLAY=:0 /usr/local/bin/processing-java --sketch="/home/pi/src/vrniture/processing/{0}/" --run'.format(random.choice(sketches))
+
+    print(sketch)
+    os.system(sketch)    
+
+def cleanup():
+    # TODO: find a cleaner way to do this
+    os.system('killall java')
+
+def brightness():
+    return True
+
+def get_part_of_day(hour):
+    return (
+        "morning" if 5 <= hour <= 11
+    else
+        "afternoon" if 12 <= hour <= 17
+    else
+        "evening" if 18 <= hour <= 22
+    else
+        "night"
+    )
+
+def shutdown_at_nighttime():
+    if get_part_of_day(datetime.now().hour) == 'night'
+        if random.randint(0, 10) < 8:
+            sys.exit()
+
+if __name__ == "__main__":
+    main()
