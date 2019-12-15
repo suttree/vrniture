f7787cb3b98423d1e9c3f220940cf6675821e6ad
Duncan Gough <duncan@suttree.com>
2019-12-15 11:43:04 +0000

    update run script to shutdown at nighttime

--- vrn-run.py
+++ vrn-run.py
@@ -9,8 +9,8 @@ def main():
 
     import random
 
-    # We run via cron every 5 minutes, so this means we only change infrequently
-    if random.randint(0,19) > 16:
+    # We run via cron every 24 minutes, so this means we only change infrequently
+    if random.randint(0,19) > 17:
         print("No change")
         sys.exit()
 
@@ -20,34 +20,19 @@ def main():
     day_part = get_part_of_day(datetime.now().hour)
 
     sketches = [ 
-        #'brush',
-        #'easel',
-        #'easel_mix',
-        #'easel_lines',
-        #'easel_strokes',
-        #'easel_strokes_2',
-        #'spots',
+        'colour_wipes',
         'special_branch_perlin_shapes',
-        #'floyd',
         'purple_squares',
-        #'special_branch_stage_lighting',
-        'led',
+        'special_branch_spotlights',
         'floyd_bigger',
         'special_branch_layered_squares',
         'shapes',
-        #'special_branch_layers',
+        'led',
         'plain_waves',
         'special_branch_shoreline',
         'coloured_squares',
-        'special_branch_spotlights',
-        #'plain_square',
-        'special_branch_natural_squares',
-        #'pyramid',
-        #'triad_lines',
-        'colour_wipes',
         'mcferrin_circles',
-        #'breathing',
-        #'floyd_big',
+        'special_branch_natural_squares',
     ]
 
     sketch = 'DISPLAY=:0 /usr/local/bin/processing-java --sketch="/home/pi/src/vrniture/processing/{0}/" --run'.format(random.choice(sketches))
@@ -55,9 +40,6 @@ def main():
     print(sketch)
     os.system('killall java')
     os.system(sketch)    
-
-def brightness():
-    return True
 
 def get_part_of_day(hour):
     return (
@@ -72,8 +54,7 @@ def get_part_of_day(hour):
 
 def shutdown_at_nighttime():
     if get_part_of_day(datetime.now().hour) == 'night':
-        if random.randint(0, 10) < 8:
-            sys.exit()
+        return True
 
 if __name__ == "__main__":
     main()
