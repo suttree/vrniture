89d56871326565d9a6f670739633f2ec7ee341dc
pi <duncan@suttree.com>
2019-12-15 11:58:56 +0000

    Removed call to DISPLAY:-0

--- vrn-run.py
+++ vrn-run.py
@@ -50,7 +50,7 @@ def main():
         #'floyd_big',
     ]
 
-    sketch = 'DISPLAY=:0 /usr/local/bin/processing-java --sketch="/home/pi/src/vrniture/processing/{0}/" --run'.format(random.choice(sketches))
+    sketch = '/usr/local/bin/processing-java --sketch="/home/pi/src/vrniture/processing/{0}/" --run'.format(random.choice(sketches))
 
     print(sketch)
     os.system('killall java')
