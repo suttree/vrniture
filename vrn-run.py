7127c63d475d191c38dbae3fbd1e29985b1b3c3b
pi <duncan@suttree.com>
2020-02-29 18:02:57 +0000

    bugfix

--- vrn-run.py
+++ vrn-run.py
@@ -38,7 +38,7 @@ def main():
 
     sketch = 'DISPLAY=:0 /usr/local/bin/processing-java --sketch="/home/pi/src/vrniture/processing/{0}/" --run'.format(random.choice(sketches))
 
-    now = datetime.datetime.now()
+    now = datetime.now()
     print(now.strftime("%Y-%m-%d %H:%M:%S"))
     print(sketch)
     print('-----')
