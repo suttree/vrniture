6ab954b94e5e0ca30ce4b7d3d93c4c25b5399733
Duncan Gough <duncan.gough@analogfolk.com>
2020-03-04 17:04:22 +0000

    mov0-run now kills any other movies/sketches before starting

--- mov-run.py
+++ mov-run.py
@@ -1,10 +1,14 @@
 # From https://stackoverflow.com/questions/54225192/how-to-play-a-shuffled-list-of-videos-in-omxplayer
 import os, random
 
-path = "/home/pi/src/sheffield"
-
+# hide the cursor
 os.system('setterm -cursor off')
 
+# stop any existing sketches or films
+os.system('killall omxplayer')
+os.system('killall java')
+
+path = "/home/pi/src/sheffield"
 videoList = os.listdir(path)
 random.shuffle(videoList)
 for video in videoList:
