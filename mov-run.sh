64cbcec96ff6e2b29ade906611a98109fe159b90
pi <duncan@suttree.com>
2020-03-29 10:40:04 +0100

    removed mouse cursor hiding code

--- mov-run.sh
+++ mov-run.sh
@@ -1,14 +1,6 @@
-# From https://keytosmart.com/single-board-computers/looping-video-playlist-omxplayer-raspberry-pi/
-
 #!/bin/sh
 
-# get rid of the cursor so we don't see it when videos are running
-export TERM=xterm
-setterm -term xterm -cursor off
-xdotool mousemove 800 800
-DISPLAY=:0 xdotool mousemove 800 800
-export TERM=xterm && DISPLAY=:0 xdotool mousemove 800 800
-
+# From https://keytosmart.com/single-board-computers/looping-video-playlist-omxplayer-raspberry-pi/
 # set here the path to the directory containing your videos
 VIDEOPATH="/home/pi/src/vrniture-movies/" 
 IMAGEPATH="/home/pi/src/vrniture-pictures/"
