8e6f408dd79d96c4467b9360a0ae27e9ea626274
pi <duncan@suttree.com>
2020-05-03 19:33:12 +0100

    huh

--- mov-run.py
+++ mov-run.py
@@ -12,9 +12,10 @@ os.system("kill -9 `ps aux | grep java | grep -v grep | awk '{print $2}'`")
 os.system("kill -9 `ps aux | grep processing | grep -v grep | awk '{print $2}'`")
 os.system("kill -9 `ps aux | grep PictureFrame | grep -v grep | awk '{print $2}'`")
 
+print("Running movs")
+
 os.system('/home/pi/src/vrniture/./mov-run.sh')
 
 print("mov done")
 print("running vrn-run now...")
 os.system("/usr/bin/env python /home/pi/src/vrniture/vrn-run.py")
-print("--------")
