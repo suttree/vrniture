4faa4ad3abbb4d874151ccc782046709a0885c5f
suttree <duncan@suttree.com>
2020-05-04 19:33:30 +0100

    indental

--- sketch-run.py
+++ sketch-run.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 
-import os, sys
+import os, sys, subprocess
 from datetime import datetime
 
 # hide the cursor
@@ -15,53 +15,53 @@ def main():
         print("Shut down at night time")
         sys.exit()
 
-import random
+    import random
 
-# We run via cron every 24 minutes, so this means we only change infrequently
-if random.randint(0,19) > 17:
-    print("No change")
-    sys.exit()
+    # We run via cron every 24 minutes, so this means we only change infrequently
+    if random.randint(0,19) > 17:
+        print("No change")
+        sys.exit()
 
-import os
+    import os
 
-# TODO: select scripts based on this? or dim screen...
-day_part = get_part_of_day(datetime.now().hour)
+    # TODO: select scripts based on this? or dim screen...
+    day_part = get_part_of_day(datetime.now().hour)
 
-sketches = [
-    'colour_wipes',
-    'purple_squares',
-    'special_branch_spotlights',
-    'floyd_bigger',
-    'special_branch_layered_squares',
-    'easel_strokes',
-    'spots',
-    'plain_waves',
-    'special_branch_shoreline',
-    'coloured_squares',
-    'mcferrin_circles',
-    'special_branch_natural_squares',
-]
+    sketches = [
+        'colour_wipes',
+        'purple_squares',
+        'special_branch_spotlights',
+        'floyd_bigger',
+        'special_branch_layered_squares',
+        'easel_strokes',
+        'spots',
+        'plain_waves',
+        'special_branch_shoreline',
+        'coloured_squares',
+        'mcferrin_circles',
+        'special_branch_natural_squares',
+    ]
 
-#sketch = 'DISPLAY=:0 /usr/local/bin/processing-java --sketch="/home/pi/src/vrniture/processing/{0}/" --run'.format(random.choice(sketches))
-#print(sketch)
-#os.system('killall java')
-#os.system(sketch)
+    #sketch = 'DISPLAY=:0 /usr/local/bin/processing-java --sketch="/home/pi/src/vrniture/processing/{0}/" --run'.format(random.choice(sketches))
+    #print(sketch)
+    #os.system('killall java')
+    #os.system(sketch)
 
-# Stop any existing sketches or films
-os.system("kill -9 `ps aux | grep java | grep -v grep | awk '{print $2}'`")
-os.system("kill -9 `ps aux | grep processing | grep -v grep | awk '{print $2}'`")
+    # Stop any existing sketches or films
+    os.system("kill -9 `ps aux | grep java | grep -v grep | awk '{print $2}'`")
+    os.system("kill -9 `ps aux | grep processing | grep -v grep | awk '{print $2}'`")
 
-sketch = random.choice(sketches)
+    sketch = random.choice(sketches)
 
-now = datetime.now()
-print(now.strftime("%Y-%m-%d %H:%M:%S"))
-print(sketch)
-print('-----')
+    now = datetime.now()
+    print(now.strftime("%Y-%m-%d %H:%M:%S"))
+    print(sketch)
+    print('-----')
 
-env = os.environ.copy()
-env['DISPLAY'] = ':0.0'
-subprocess.Popen(['/usr/local/bin/processing-java', '--sketch=/home/pi/src/vrniture/processing/{0}/'.format(sketch),  '--run'], env=env)
-sys.exit(0)
+    env = os.environ.copy()
+    env['DISPLAY'] = ':0.0'
+    subprocess.Popen(['/usr/local/bin/processing-java', '--sketch=/home/pi/src/vrniture/processing/{0}/'.format(sketch),  '--run'], env=env)
+    sys.exit(0)
 
 def get_part_of_day(hour):
     return (
